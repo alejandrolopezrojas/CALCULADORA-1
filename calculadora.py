9c375ed956091b6d6b639d1effce24febfe07318
alejandrolopezrojas <alejandrolopezrojas2009@gmail.com>
2026-01-10 15:01:40 -0500

    segunda variable, selección de operación

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,20 @@
 print("digite los dos numeros que desea operar")
 x1 = int(input("numero uno= "))
 x2 = int(input("numero dos= "))
+op = input("""1 = +
+2 = -
+3 = *
+4 = /
+5 = //
+6 = %
+7 = **
+Elija que operación desea realizar= """)
+
+
+
+
+
+
+
+
+
